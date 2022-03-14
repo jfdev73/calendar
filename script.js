b15c875b918d7ea588f61e9dd4185caa41d9cf47
JUAN FERNANDO MIRANDA MONROY <becario.fernando@subsecretaria.dominio>
2022-03-14 17:46:11 -0600

    modificaciones de estilo del datepicker

---
+++ script.js
@@ -0,0 +1,13 @@
+$('.datepicker3').datepicker({
+
+    container: '.container-calendar',
+    autoclose: false,
+    multidate:true,
+    todayHighlight: true,
+    format: 'dd/mm/yyyy',
+    language: 'es',
+    daysOfWeekDisabled:"0,6",
+    weekStart:0
+   
+
+});
